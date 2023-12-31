d6ed1a69f86c9410e164da3f237f668d918fdaa2
Sayani Nethmi <nethmihewage2002@gmail.com>
2023-12-31 17:40:03 +0530

    updated

--- assets/js/script.js
+++ assets/js/script.js
@@ -11,3 +11,12 @@ const modalCloseFunc = function (){ modal.classList.add('closed')}
 //modal eventListener
 modalCloseOverlay.addEventListener('click',modalCloseFunc);
 modalCloseBtn.addEventListener('click',modalCloseFunc);
+
+// notification toast variables
+const notificationToast = document.querySelector('[data-toast]');
+const toastCloseBtn = document.querySelector('[data-toast-close]');
+
+// notification toast eventListener
+toastCloseBtn.addEventListener('click',function (){
+    notificationToast.classList.add('closed');
+});
