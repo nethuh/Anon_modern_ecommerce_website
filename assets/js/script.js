78ff0bf167c65f56bd77de40643f49da07c320f7
Sayani Nethmi <nethmihewage2002@gmail.com>
2024-01-26 16:47:50 +0530

    updated

--- assets/js/script.js
+++ assets/js/script.js
@@ -45,3 +45,34 @@ for (let i = 0; i < mobileMenuOpenBtn.length; i++) {
     overlay.addEventListener('click', mobileMenuCloseFunc);
 
 }
+
+
+// accordion variables
+const accordionBtn = document.querySelectorAll('[data-accordion-btn]');
+const accordion = document.querySelectorAll('[data-accordion]');
+
+for (let i = 0; i < accordionBtn.length; i++) {
+
+    accordionBtn[i].addEventListener('click', function () {
+
+        const clickedBtn = this.nextElementSibling.classList.contains('active');
+
+        for (let i = 0; i < accordion.length; i++) {
+
+            if (clickedBtn) break;
+
+            if (accordion[i].classList.contains('active')) {
+
+                accordion[i].classList.remove('active');
+                accordionBtn[i].classList.remove('active');
+
+            }
+
+        }
+
+        this.nextElementSibling.classList.toggle('active');
+        this.classList.toggle('active');
+
+    });
+
+}
